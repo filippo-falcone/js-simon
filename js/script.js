ebb921948373c11500f65ab6c3f6c6d1893a4d68
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-20 13:40:50 +0100

    feat: add function to generate array

--- js/script.js
+++ js/script.js
@@ -10,3 +10,29 @@
 // --- li pusho
 // Stampo all'utente quanti numeri numeri ha indovinato e quali sono
 */
+/* MATERIAL */
+const domText = document.querySelector('#number');
+const guessNumbers = generatorRndNumbArray(5, 1, 100);
+/* BODY */
+domText.innerHTML = guessNumbers;
+/* FUNCTIONS */
+// Funzione che genera un array di numeri casuali unici
+// arrayLenght: elemento numerico che rappresenta la lunghezza massima dell'array
+// min: elemento numerico che rappresenta il numero minimo dell'intervallo dei numeri casuali
+// max: elemento numerico che rappresenta il numero massimo dell'intervallo dei numeri casuali
+// return: rndArray array di numeri casuali
+function generatorRndNumbArray(arrayLength, min, max) {
+    let rndArray = [];
+    while (rndArray.length < arrayLength) {
+        const rndNumber = getRndInteger(min, max);
+        if (!rndArray.includes(rndNumber)) {
+            rndArray.push(rndNumber);
+        }
+    }
+    return rndArray;
+}
+// Funzione che genera numeri random
+// Presa da w3schools
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
