2c1ca9527189f9cca25a9ab298dbeec48e3a6323
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-20 13:19:41 +0100

    Let's a Go

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+// Creo un array 'guessNumbers' di 5 numeri casuali
+// Stampo sul DOM l'array
+// Creo un setTimer di 30000 ms in cui
+// - Pulisco il dom
+// - Creo un array 'enteredNumbers'
+// - Chiedo all'utente di inserie 5 numeri
+// - Inserisco questi numeri in 'enteredNumbers'
+// -- Se i numeri inseriti sono inclusi nell'array 'guessNumbers' e non sono già presenti nell'array
+// --- li pusho
+// Stampo all'utente quanti numeri numeri ha indovinato e quali sono
+*/
