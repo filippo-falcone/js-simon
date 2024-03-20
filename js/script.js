39a8a0a038e29f350fe6be59adf73d267fa64eae
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-20 15:08:37 +0100

    add: setTime function playGame

--- js/script.js
+++ js/script.js
@@ -16,14 +16,29 @@ const domText = document.querySelector('#number');
 const guessNumbers = generatorRndNumbArray(5, 1, 100);
 /* BODY */
 domText.innerHTML = guessNumbers;
-setTimeout(cleanDom, 2000)
+setTimeout(cleanDom, 30000);
+setTimeout(playGame, 31000);
 /* FUNCTIONS */
 // Funzione richiamata in un setTimeout che pulisce il DOM
 function cleanDom() {
     domText.innerHTML = '';
 }
+// Funzione richiamata in un setTimeout che fa partire il gioco
+function playGame() {
+    const enteredNumbers = [];
+    for (let i = 0; i < 5; i++) {
+        const userNumber = parseInt(prompt('Inserisci un numero:'));
+        if (isNaN(userNumber)) {
+            alert('Il numero non è valido');
+            i--;
+        } else if (guessNumbers.includes(userNumber) && !enteredNumbers.includes(userNumber)) {
+            enteredNumbers.push(userNumber);
+        }
+    }
+    domText.innerHTML = `Hai indovinato ${enteredNumbers.length} numeri: ${enteredNumbers}`;
+}
 // Funzione che genera un array di numeri casuali unici
-// arrayLenght: elemento numerico che rappresenta la lunghezza massima dell'array
+// arrayLength: elemento numerico che rappresenta la lunghezza massima dell'array
 // min: elemento numerico che rappresenta il numero minimo dell'intervallo dei numeri casuali
 // max: elemento numerico che rappresenta il numero massimo dell'intervallo dei numeri casuali
 // return: rndArray array di numeri casuali
